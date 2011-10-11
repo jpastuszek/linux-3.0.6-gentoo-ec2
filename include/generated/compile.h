82ad9edcdab0d7775df50c367e485fee2d44b83f
Jakub Pastuszek <jpastuszek@gmail.com>
2011-10-11 15:17:47 +0200

    ec2 working kernel 3.0.6 gentoo

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Tue Oct 11 14:57:38 CEST 2011"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "magi"
+#define LINUX_COMPILER "gcc version 4.5.2 (Gentoo 4.5.2 p1.1, pie-0.4.5) "
